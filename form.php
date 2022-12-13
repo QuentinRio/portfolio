60790322ff70d498068ce1e897d8c0ac39a7f0a9
QuentinRio <quentin.rio@gmail.com>
2022-12-13 16:04:25 +0100

    pb contact

---
+++ form.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = "";
+$email = "";
+$sujet = "";
+$message = "";
+
+//if(isset($_POST['submit'])){
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['sujet']) && isset($_POST['message'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $sujet = $_POST['sujet'];
+    $message = $_POST['message'];
+
+
+    $retour = mail('quentin.rio@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: quentin.rio@hotmail.fr');
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+}
+
+
+
+?>
